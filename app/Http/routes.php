90cb0b6d2ab7d437872ca8494caed9430add0f85
Narendra Chitrakar <developer.naren@gmail.com>
2016-05-05 19:57:49 +0545

    adds new routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,29 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'api', function() {
+
+
+    Route::get('product', 'ProductController@index');
+    Route::post('product', 'ProductController@create');
+    Route::get('product/{slug}', 'ProductController@show');
+    Route::post('product/{slug}', 'ProductController@update');
+    Route::delete('product/{slug}','ProductController@delete');
+
+    Route::get('cart', 'CartController@index');
+    Route::post('cart', 'CartController@create');
+    Route::get('cart/{id}', 'CartController@show');
+    Route::post('cart/{id}', 'CartController@update');
+    Route::delete('cart/{id}','CartController@delete');
+
+    Route::get('user', 'UserController@index');
+    Route::post('user', 'UserController@create');
+    Route::get('user/{id}', 'UserController@show');
+    Route::post('user/{id}', 'UserController@update');
+    Route::delete('user/{id}','UserController@delete');
+
+}]);
